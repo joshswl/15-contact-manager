5039ab67f6c4400ffdd53d3b53cc40cd3c56ae07
Josh Lee <joshswl@gmail.com>
2017-02-27 21:21:22 -0600

    progress on tests

--- app/reducer.js
+++ app/reducer.js
@@ -1,3 +1,10 @@
-export default function reducer () {
-
+export default function reducer (state, action) {
+  switch (action.type) {
+    case 'CONTACT@CREATE':
+      return { contacts: [action.data, ...state.contacts] };
+    case 'CONTACT@FIND_ALL':
+      return { contacts: action.data };
+    default:
+      return state || { contacts: [] };
+  }
 }
--- tests/reducer-test.js
+++ tests/reducer-test.js
@@ -5,13 +5,11 @@ module('reducer', () => {
     assert.ok(reducer, 'reducer exists');
   });
 
-  test('add contact', (assert) => {
-    const oldState = { contacts: [] };
-    const firstState = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+  test('add contact', () => {
+    const oldStateOne = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+    const oldStateTwo = { contacts: [] };
     const actionOne = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'John', lastName: 'Cena' }] };
     const actionTwo = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'Nic', lastName: 'Cage' }] };
-    assert.deepEqual(reducer(oldState, actionOne), { contacts: [{ firstName: 'John', lastName: 'Cena' }] });
-    assert.deepEqual(reducer(firstState, actionOne), { contacts: [{ firstName: 'John', lastName: 'Cena' }] });
-    assert.deepEqual(reducer(firstState, actionTwo), { contacts: [{ firstName: 'John', lastName: 'Cena' }] });
-  });
+    const actionThree = { type: 'CONTACT@CREATE', data: { firstName: 'Johnny', lastName: 'Depp' } };
+  })
 });
