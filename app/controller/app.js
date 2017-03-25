e8fe40d35a989c6ea3cbf98b17304b7f04855138
Josh Lee <joshswl@gmail.com>
2017-03-25 19:12:24 -0400

    reorganized and updated constructor

--- app/controller/app.js
+++ app/controller/app.js
@@ -1,7 +1,12 @@
+import FormView from '../view/contact-form';
+import ListView from '../view/contact-list';
+
 export default class AppController {
   constructor(element, store) {
     this.element = element;
     this.store = store;
+    this.FormView = new FormView(element.querySelector('.contact-info'), store);
+    this.ListView = new ListView(element.querySelector('.grid'), store);
   }
   created() {
     this.store.subscribe(() => {
---
+++ app/view/contact-form.js
@@ -0,0 +1,23 @@
+export default class ContactFormView {
+  constructor(element, store) {
+    this.element = element;
+    this.store = store;
+  }
+
+  mounted() {
+    this.element.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      this.store.dispatch({
+        type: 'CONTACT@CREATE',
+        contacts: {
+          first: this.element.querySelector('.first-name').value,
+          last: this.element.querySelector('.last-name').value,
+          street: this.element.querySelector('.street').value,
+          city: this.element.querySelector('.city').value,
+          state: this.element.querySelector('.state').value,
+        }
+      });
+    });
+  }
+}
